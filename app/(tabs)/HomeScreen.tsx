622c5b38267114a90449c1fde5b194072b2719c1
JuliannyAraujo <rm99554@fiap.com.br>
2024-06-06 21:23:19 -0300

    imagem oceano

--- app/(tabs)/HomeScreen.tsx
+++ app/(tabs)/HomeScreen.tsx
@@ -1,26 +1,14 @@
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      {/* Barra de pesquisa */}
-      <View style={styles.searchBar}>
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Pesquisar..."
-          placeholderTextColor="#666"
-        />
-        <TouchableOpacity style={styles.searchButton}>
-          <Ionicons name="search" size={24} color="#007bff" />
-        </TouchableOpacity>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Salvem os oceanos</Text>
       </View>
 
-      {/* Título e Categorias */}
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Categorias</Text>
-      </View>
       <View style={styles.categoriesContainer}>
         {/* Ícones e texto das categorias */}
         <TouchableOpacity onPress={() => alert('Aviso')} style={styles.categoryItem}>
@@ -37,7 +25,12 @@ export default function HomeScreen() {
         </TouchableOpacity>
       </View>
 
-      {/* Texto sobre a poluição nos oceanos */}
+      <View style={styles.conscientizationContainer}>
+        <Text style={styles.conscientizationText}>
+          A poluição nos oceanos é um problema crescente que afeta diretamente a vida marinha e o equilíbrio dos ecossistemas. Junte-se a nós na luta pela conscientização e preservação dos nossos preciosos recursos marinhos.
+        </Text>
+      </View>
+
       <Text style={styles.paragraph}>
         O oceano carrega consigo um fardo alarmante: cerca de 2.3 milhões de toneladas de plástico flutuam em suas águas, com o Brasil contribuindo com impressionantes 325 mil toneladas anualmente. Esta é uma carga que não só afeta a vida marinha, mas também causa impacto direto nas comunidades costeiras e no delicado equilíbrio dos ecossistemas marinhos.
       </Text>
@@ -47,39 +40,15 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E0F2F1', // Azul claro
     paddingVertical: 20,
     paddingHorizontal: 16,
   },
-  searchBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  searchInput: {
-    flex: 1,
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginRight: 10,
-    fontSize: 16,
-  },
-  searchButton: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#007bff',
-  },
-  titleContainer: {
+  header: {
     alignItems: 'center',
     marginBottom: 24,
   },
-  title: {
+  headerText: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#007bff',
@@ -97,6 +66,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#007bff',
     marginTop: 8,
+  },
+  conscientizationContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  conscientizationText: {
+    fontSize: 16,
+    textAlign: 'justify',
   },
   paragraph: {
     fontSize: 16,
